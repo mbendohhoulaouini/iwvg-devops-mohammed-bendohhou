f6823205eef945189cb3f87790bf3231f1430311
mbendohhoulaouini <mohammed.bendohhou.laouini@alumnos.upm.es>
2020-10-03 22:54:29 +0200

    Create entity Fraction and Test #7

---
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -0,0 +1,47 @@
+package es.upm.miw.iwvg_devops.code;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FractionTest {
+
+    private Fraction fraction;
+
+    @BeforeEach
+    void before() {
+        fraction = new Fraction(2, 3);
+    }
+
+    @Test
+    void testFractionIntInt() {
+        assertEquals(2, fraction.getNumerator());
+        assertEquals(3, fraction.getDenominator());
+    }
+
+    @Test
+    void testFractionInt() {
+        fraction = new Fraction();
+        assertEquals(1, fraction.getNumerator());
+        assertEquals(1, fraction.getDenominator());
+    }
+
+    @Test
+    void testFractionDecimalInt() {
+        fraction = new Fraction();
+        assertEquals(1, fraction.decimal());
+    }
+
+    @Test
+    void testFractionDecimalPeriodicDouble() {
+        assertEquals(0.66, fraction.decimal(), 10e-3);
+    }
+
+    @Test
+    void testFractionConvertDecimalError() {
+        fraction = new Fraction(1, 0);
+        assertEquals(0, fraction.getDenominator() / fraction.getNumerator());
+    }
+
+}
