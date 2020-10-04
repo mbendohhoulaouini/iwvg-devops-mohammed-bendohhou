0fa975880e177251ff89626463715de89e1f0a86
mbendohhoulaouini <mohammed.bendohhou.laouini@alumnos.upm.es>
2020-10-04 21:42:35 +0200

    Define One search #12

---
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -0,0 +1,37 @@
+package es.upm.miw.iwvg_devops.code;
+
+import org.apache.logging.log4j.LogManager;
+
+import java.util.stream.Stream;
+
+public class Searches {
+
+    public Stream<String> findUserFamilyNameByUserNameDistinct(String userName) {
+        return new UsersDatabase().findAll()
+                .filter(user -> userName.equals(user.getName()))
+                .map(User::getFamilyName)
+                .distinct();
+    }
+
+    public Stream<Integer> findFractionNumeratorByUserFamilyName(String userFamilyName) {
+        return new UsersDatabase().findAll()
+                .peek(x -> LogManager.getLogger(this.getClass()).info("before: " + x))
+                .filter(user -> userFamilyName.equals(user.getFamilyName()))
+                .peek(x -> LogManager.getLogger(this.getClass()).info("after: " + x))
+                .flatMap(user -> user.getFractions().stream())
+                .map(Fraction::getNumerator);
+    }
+
+    public Stream<String> findUserFamilyNameByFractionDenominator(int fractionDenominator) {
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getFractions().stream()
+                        .anyMatch(fraction -> fractionDenominator == fraction.getDenominator()))
+                .map(User::getFamilyName);
+    }
+
+    public Stream<Double> findDecimalImproperFractionByUserName(String name) {
+        return new UsersDatabase().findAll().filter(user -> name.equals(user.getName()))
+                .flatMap(user -> user.getFractions().stream().filter(Fraction::isImproper)).map(Fraction::decimal);
+
+    }
+}
